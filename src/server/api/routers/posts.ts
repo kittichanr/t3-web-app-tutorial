a73db3f736a94c1c06089df35385f654ba36c5ad
petchkubb <petchkubbb@gmail.com>
2023-05-20 15:23:13 +0700

    Add Create Post

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,44 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "y/server/api/trpc";
+import { type User, clerkClient } from "@clerk/nextjs/server";
+import { TRPCClientError } from "@trpc/client";
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+    return {
+        id: user.id,
+        username: user.username,
+        profilePicture: user.profileImageUrl
+    }
+}
 
 export const postsRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.post.findMany();
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const posts = await ctx.prisma.post.findMany({
+            take: 100,
+        });
+
+        const users = (await clerkClient.users.getUserList({
+            userId: posts.map(post => post.authorId),
+            limit: 100
+        })).map(filterUserForClient)
+
+        return posts.map(post => {
+            const author = users.find(user => user.id === post.authorId)
+
+            if (!author || !author.username) throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: 'Author for post not found'
+            })
+
+            return {
+                post,
+                author: {
+                    ...author,
+                    username: author.username
+                }
+            }
+        })
     }),
 });
